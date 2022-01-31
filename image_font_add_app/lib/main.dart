6b42ce645e08deec47c9733aca36523fb6482167
bigzero <garack@gmail.com>
2022-01-31 11:12:59 +0900

    finish page 101

---
+++ image_font_add_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:image_font_add_app/imageWidget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ImageWidgetApp(),
+    );
+  }
+}
